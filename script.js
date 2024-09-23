b1569bd54317f1568bffbadd3de014aaef5cb2c5
Filipe Formigari <formigari2804@gmail.com>
2024-09-23 15:14:36 -0300

    a

---
+++ script.js
@@ -0,0 +1,18 @@
+const botao = document.getElementById("botao")
+const qrCode = document.getElementById("QRcode")
+
+botao.addEventListener("click", (e) =>{
+
+    e.preventDefault();
+    qrCode.innerHTML = "";
+
+    const texto = document.createElement("p");
+    texto.innerText = "Gerando QRcode..."
+
+
+    const img = document.createElement("img");
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`;
+    qrCode.appendChild(img);
+
+    console.log("mudou");
+})

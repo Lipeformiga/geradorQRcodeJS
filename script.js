baf84078d4103c95a0bbb8bc6c45ec1f0b8554d4
Filipe Formigari <formigari2804@gmail.com>
2024-09-23 16:21:45 -0300

    asda

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const botao = document.getElementById("botao")
 const qrCode = document.getElementById("QRcode")
+const input = document.getElementById("input")
 
-botao.addEventListener("click", (e) =>{
+botao.addEventListener("click", () =>{
 
-    e.preventDefault();
     qrCode.innerHTML = "";
-
-    botao.innerText = ""
     const texto = document.createElement("p");
-    texto.innerText = "Gerando QRcode..."
+    botao.innerText = "Gerando QRcode..."
     botao.appendChild(texto)
 
-
     const img = document.createElement("img");
-    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`;
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${(input.value)}`;
     qrCode.appendChild(img);
 
-    console.log("mudou");
+    img.addEventListener("load", () => {
+        botao.innerText = "Gerar outro QRcode"
+    })
 })
-
